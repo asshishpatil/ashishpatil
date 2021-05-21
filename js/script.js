6908eb13c9df35343f73bd92cff93721ec77e0cc
asshishpatil <84504997+asshishpatil@users.noreply.github.com>
2021-05-21 12:13:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ shuffleCards = (cards) => {
 */
 sortAndShuffle  = (sort = false) => {
 	cardRef 	= (!Boolean(cardRef)) ? document.getElementById("Cards") : cardRef;  
-	sortedCards = (!Boolean(sortedCards)) ? cardRef.children : sortedCards;
+	sortedCards = (!Boolean(sortedCards)) ? [...cardRef.children] : sortedCards;
 	let cards 	= (sort) ? sortedCards : shuffleCards(cardRef.children);
 	for(let i = 0; i < cards.length; i++) {
         cardRef.appendChild(cards[i]);
